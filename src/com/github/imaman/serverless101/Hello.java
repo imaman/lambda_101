7216fed6f2f9e0013c808c30568fed3489877994
Itay Maman <imaman@google.com>
2017-10-23 21:56:02 +0300

    canonical example (modulo some changes)

---
+++ src/com/github/imaman/serverless101/Hello.java
@@ -0,0 +1,28 @@
+package com.github.imaman.serverless101;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.amazonaws.services.lambda.runtime.Context;
+
+public class Hello {
+  public Map<String, Object> handleRequest(Map<String, Object> map, Context context) {
+    Map<String, Object> ret = new HashMap<>();
+
+    ret.put("statusCode", "200");
+
+    String input = map.entrySet().stream().map(x -> x.toString())
+        .collect(Collectors.joining("<br>\n"));
+    String body = "<html><body><h1>AWS Lambda 101, we are at $DATE$ (Map generated!)</h1>\n<h2>$IN$</h2>\n</body></html>\n";
+    body = body.replace("$DATE$", new Date().toString()).replace("$IN$", input);
+    ret.put("body", body);
+
+    Map<String, Object> headers = new HashMap<>();
+    headers.put("Content-Type", "text/html");
+    ret.put("headers", headers);
+
+    return ret;
+  }
+}
