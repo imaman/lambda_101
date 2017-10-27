63dfaed8e276de18b36f5e899b802a3f8e0fe50b
Itay Maman <imaman@google.com>
2017-10-27 19:42:13 +0300

    rename the entry point class

--- src/com/github/imaman/lambda101/LambdaMain.java
+++ src/com/github/imaman/lambda101/LambdaMain.java
@@ -6,13 +6,13 @@ import java.util.Map;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class Hello implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+public class LambdaMain implements RequestHandler<Map<String, Object>, Map<String, Object>> {
   public Map<String, Object> handleRequest(Map<String, Object> map, Context context) {
     Map<String, Object> ret = new HashMap<>();
 
     ret.put("statusCode", "200");
     String body = String.format(
-        "<html><body><h1>AWS Lambda is up and running</h1><h2>#inputs=%s</h2></body></html>\n",
+        "<html><body><h1>AWS Lambda (" + getClass().getName() + ") is up and running</h1><h2>#Inputs=%s</h2></body></html>\n",
         map.size());
     ret.put("body", body);
 
